1acb7b5acb984d671a53941580f69b0edf32bf7c
Mantas Kuncevičius <m.kuncevicius@gmail.com>
2019-02-08 16:22:03 +0200

    Added functions which create nodes in OPC UA server address space of methods: runTerminalCmd, openTerminal, closeTerminal.(Now returning BadNotImplemented status code)

--- opcua-remote-control-server/myserver.cpp
+++ opcua-remote-control-server/myserver.cpp
@@ -1,5 +1,5 @@
 #include "myserver.h"
-#include "open62541.h"
+
 #include <signal.h>
 
 UA_Boolean running = true;
@@ -10,7 +10,11 @@ static void stopHandler(int sig) {
 
 MyServer::MyServer()
 {
+}
 
+MyServer::~MyServer()
+{
+    UA_Server_delete(m_server);
 }
 
 bool MyServer::startServer()
@@ -20,12 +24,119 @@ bool MyServer::startServer()
 
     // Create a server listening on port 4840
     UA_ServerConfig *config = UA_ServerConfig_new_default();
-    UA_Server *server = UA_Server_new(config);
+    m_server = UA_Server_new(config);
+
+    // Add object
+    UA_ObjectAttributes oAttr = UA_ObjectAttributes_default;
+    oAttr.displayName = UA_LOCALIZEDTEXT("en-US", "SystemObj");
+    UA_Server_addObjectNode(m_server, UA_NODEID_NULL,
+                            UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER),
+                            UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES),
+                            UA_QUALIFIEDNAME(1, "SystemObj"), UA_NODEID_NUMERIC(0, UA_NS0ID_BASEOBJECTTYPE),
+                            oAttr, NULL, &m_systemObjId);
+
+    // Add methods
+    addOpenTerminalMethod(m_server);
+    addRunTerminalCmdMethod(m_server);
+    addCloseTerminalMethod(m_server);
 
     //Run the server loop
-    UA_StatusCode retval = UA_Server_run(server, &running);
-    UA_Server_delete(server);
+    UA_StatusCode retval = UA_Server_run(m_server, &running);
     UA_ServerConfig_delete(config);
 
     return (int)retval;
 }
+
+UA_StatusCode MyServer::openTerminalMethodCallback(UA_Server *server,
+                         const UA_NodeId *sessionId, void *sessionHandle,
+                         const UA_NodeId *methodId, void *methodContext,
+                         const UA_NodeId *objectId, void *objectContext,
+                         size_t inputSize, const UA_Variant *input,
+                         size_t outputSize, UA_Variant *output) {
+
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "Method is not implemented yet!");
+    return UA_STATUSCODE_BADNOTIMPLEMENTED;
+}
+
+void MyServer::addOpenTerminalMethod(UA_Server *server) {
+
+    UA_Argument inputArgument;
+    UA_Argument_init(&inputArgument);
+
+    UA_Argument outputArgument;
+    UA_Argument_init(&outputArgument);
+
+    UA_MethodAttributes openTerminalAttr = UA_MethodAttributes_default;
+    openTerminalAttr.description = UA_LOCALIZEDTEXT("en-US","Method to open terminal");
+    openTerminalAttr.displayName = UA_LOCALIZEDTEXT("en-US","Open Terminal");
+    openTerminalAttr.executable = true;
+    openTerminalAttr.userExecutable = true;
+    UA_Server_addMethodNode(server, UA_NODEID_NULL, m_systemObjId,
+                            UA_NODEID_NUMERIC(0, UA_NS0ID_HASORDEREDCOMPONENT),
+                            UA_QUALIFIEDNAME(1, "openTerminal"),
+                            openTerminalAttr, &openTerminalMethodCallback,
+                            1, &inputArgument, 1, &outputArgument, NULL, NULL);
+}
+
+UA_StatusCode MyServer::runTerminalCmdMethodCallback(UA_Server *server,
+                         const UA_NodeId *sessionId, void *sessionHandle,
+                         const UA_NodeId *methodId, void *methodContext,
+                         const UA_NodeId *objectId, void *objectContext,
+                         size_t inputSize, const UA_Variant *input,
+                         size_t outputSize, UA_Variant *output) {
+
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "Method is not implemented yet!");
+    return UA_STATUSCODE_BADNOTIMPLEMENTED;
+}
+
+void MyServer::addRunTerminalCmdMethod(UA_Server *server) {
+
+    UA_Argument inputArgument;
+    UA_Argument_init(&inputArgument);
+
+    UA_Argument outputArgument;
+    UA_Argument_init(&outputArgument);
+
+    UA_MethodAttributes runTerminalCmdAttr = UA_MethodAttributes_default;
+    runTerminalCmdAttr.description = UA_LOCALIZEDTEXT("en-US","Method to run terminal cmd");
+    runTerminalCmdAttr.displayName = UA_LOCALIZEDTEXT("en-US","Run Terminal Cmd");
+    runTerminalCmdAttr.executable = true;
+    runTerminalCmdAttr.userExecutable = true;
+    UA_Server_addMethodNode(server, UA_NODEID_NULL, m_systemObjId,
+                            UA_NODEID_NUMERIC(0, UA_NS0ID_HASORDEREDCOMPONENT),
+                            UA_QUALIFIEDNAME(1, "runTerminalCmd"),
+                            runTerminalCmdAttr, &runTerminalCmdMethodCallback,
+                            1, &inputArgument, 1, &outputArgument, NULL, NULL);
+}
+
+UA_StatusCode MyServer::closeTerminalMethodCallback(UA_Server *server,
+                         const UA_NodeId *sessionId, void *sessionHandle,
+                         const UA_NodeId *methodId, void *methodContext,
+                         const UA_NodeId *objectId, void *objectContext,
+                         size_t inputSize, const UA_Variant *input,
+                         size_t outputSize, UA_Variant *output) {
+
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "Method is not implemented yet!");
+    return UA_STATUSCODE_BADNOTIMPLEMENTED;
+}
+
+void MyServer::addCloseTerminalMethod(UA_Server *server) {
+
+    UA_Argument inputArgument;
+    UA_Argument_init(&inputArgument);
+
+    UA_Argument outputArgument;
+    UA_Argument_init(&outputArgument);
+
+    UA_MethodAttributes closeTerminalAttr = UA_MethodAttributes_default;
+    closeTerminalAttr.description = UA_LOCALIZEDTEXT("en-US","Method to close terminal");
+    closeTerminalAttr.displayName = UA_LOCALIZEDTEXT("en-US","Close Terminal");
+    closeTerminalAttr.executable = true;
+    closeTerminalAttr.userExecutable = true;
+    UA_Server_addMethodNode(server, UA_NODEID_NULL, m_systemObjId,
+                            UA_NODEID_NUMERIC(0, UA_NS0ID_HASORDEREDCOMPONENT),
+                            UA_QUALIFIEDNAME(1, "closeTerminal"),
+                            closeTerminalAttr, &closeTerminalMethodCallback,
+                            1, &inputArgument, 1, &outputArgument, NULL, NULL);
+}
+
--- opcua-remote-control-server/myserver.h
+++ opcua-remote-control-server/myserver.h
@@ -1,13 +1,43 @@
 #ifndef MYSERVER_H
 #define MYSERVER_H
 
+#include "open62541.h"
 
 class MyServer
 {
 public:
     MyServer();
+    ~MyServer();
 
     bool startServer();
+
+    static UA_StatusCode openTerminalMethodCallback(UA_Server *server,
+                             const UA_NodeId *sessionId, void *sessionHandle,
+                             const UA_NodeId *methodId, void *methodContext,
+                             const UA_NodeId *objectId, void *objectContext,
+                             size_t inputSize, const UA_Variant *input,
+                             size_t outputSize, UA_Variant *output);
+    void addOpenTerminalMethod(UA_Server *server);
+
+    static UA_StatusCode runTerminalCmdMethodCallback(UA_Server *server,
+                             const UA_NodeId *sessionId, void *sessionHandle,
+                             const UA_NodeId *methodId, void *methodContext,
+                             const UA_NodeId *objectId, void *objectContext,
+                             size_t inputSize, const UA_Variant *input,
+                             size_t outputSize, UA_Variant *output);
+    void addRunTerminalCmdMethod(UA_Server *server);
+
+    static UA_StatusCode closeTerminalMethodCallback(UA_Server *server,
+                             const UA_NodeId *sessionId, void *sessionHandle,
+                             const UA_NodeId *methodId, void *methodContext,
+                             const UA_NodeId *objectId, void *objectContext,
+                             size_t inputSize, const UA_Variant *input,
+                             size_t outputSize, UA_Variant *output);
+    void addCloseTerminalMethod(UA_Server *server);
+
+private:
+    UA_NodeId m_systemObjId; // Get the nodeid assigned by the server
+    UA_Server *m_server;
 };
 
 #endif // MYSERVER_H
